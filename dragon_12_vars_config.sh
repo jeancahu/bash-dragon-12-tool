3e0a8880573fe3237ccfb2a1f6402dba01724b60
Jeancarlo Hidalgo <jeancahu@gmail.com>
2017-11-17 13:45:11 -0600

    Config files

---
+++ dragon_12_vars_config.sh
@@ -0,0 +1,15 @@
+
+# Dragon 12 configuration file
+#
+
+export DRAGON_AS12_PATH="$HOME/.dragon_12/HCS12Text"
+export DRAGON_AS=as12.exe
+export DRAGON_RAM_BEG=0X1000
+export DRAGON_RAM_END=0X3FFF
+export DRAGON_SERIAL_PORT=/dev/ttyUSB0
+export DRAGON_SERIAL_SPEED=9600
+export DRAGON_SESSION_NAME=dragon12
+export DRAGON_SIMULATOR_PATH="$HOME/.dragon_12/simulator"
+export DRAGON_SIMULATOR=Micro.jar
+export DRAGON_WAIT_SEND_LINE=0.04
+export DRAGON_WAIT_COMMAND=0.30
