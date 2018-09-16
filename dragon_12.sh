259daad9ea4da2cdfa8d5a227fc899b09de50fa3
hgeihsen <hgeihsen@pong>
2018-09-15 21:56:25 -0600

    add: dev: Add simulator config and flag

--- dragon_12.sh
+++ dragon_12.sh
@@ -73,7 +73,7 @@ function echo_help ()
 
     -h				Show this message, help option
     -f		<file.asm>   	Indicate <file>.asm script input
-    -l		<file.lst>	Define list file out name 
+    -l		<file.lst>	Define list file out name
     -o		<file.s19>	Define objet file out name
     -a				Use asm12 to create the object file
     -b				Send the object file to board
@@ -86,8 +86,9 @@ function echo_help ()
     -C				Send a char/string to serial TTY, this does permit spaces,
     				control chars are:
 					      S	      Space
-					      R	      CarrieReturn	
+					      R	      CarrieReturn
 					      E	      Exit loop
+    -s		<file.s19>	Load .s19 file to simulate with
     -S				Open TTY serial access to communicate with board through Terminal
 
 Report bugs to: Jeancarlo Hidalgo U. <jeancahu@gmail.com>'
@@ -111,6 +112,7 @@ declare -l BURN      #false
 declare -l RUN       #false
 declare -l SENDCHAR  #false
 declare -l SCHARLOOP #false
+declare -l SIM       #false
 declare -l INIT_TTY  #false
 
 ## Define vars
@@ -132,9 +134,10 @@ if (( $( grep -c 'a' <<< "$FLAGS" ) )); then ASSEMBLY=true  ; fi
 if (( $( grep -c 'b' <<< "$FLAGS" ) )); then BURN=true      ; fi
 if (( $( grep -c 'c' <<< "$FLAGS" ) )); then SENDCHAR=true  ; fi
 if (( $( grep -c 'C' <<< "$FLAGS" ) )); then SCHARLOOP=true ; fi
+if (( $( grep -c 's' <<< "$FLAGS" ) )); then SIM=true       ; fi
 if (( $( grep -c 'S' <<< "$FLAGS" ) )); then INIT_TTY=true  ; fi
 
-if [ $( sed 's/[hflogabcCS-]//g;s/ //g' <<< "$FLAGS" ) ]
+if [ $( sed 's/[hflogabcCsS-]//g;s/ //g' <<< "$FLAGS" ) ]
 then
     echo_error "The expression $( sed 's/[hflogabcCS]//g;s/- //g' <<< $FLAGS ) has not been recognized"
     exit $EFNR
@@ -167,12 +170,12 @@ then
 	You can add your user to Unix-to-Unix devices group too"
 	exit $ESPNWP
     fi
-    
+
     if [ -z "$( screen -ls | grep -v '^No' | grep -o $DRAGON_SESSION_NAME )" ]
     then
 	screen -S $DRAGON_SESSION_NAME $DRAGON_SERIAL_PORT $DRAGON_SERIAL_SPEED
 	exit 0
-    else	
+    else
 	screen -x $DRAGON_SESSION_NAME
 	exit 0
     fi
@@ -194,7 +197,7 @@ fi
 
 if [ $LST ] # FILE.lst
 then
-    LFILE="$( echo $* | grep -o '\-[a-z]\{0,\}l [\#0-9a-zA-Z/\.\_\ -]*\.lst' | cut --delimiter=' ' -f 2- )"    
+    LFILE="$( echo $* | grep -o '\-[a-z]\{0,\}l [\#0-9a-zA-Z/\.\_\ -]*\.lst' | cut --delimiter=' ' -f 2- )"
 
     if [ -z "$LFILE" ]
     then
@@ -205,12 +208,12 @@ then
     fi
 
     echo -e "List file name: $ANSI_GREEN""$LFILE""$ANSI_NOCOLOR"
-    
+
 fi
 
 if [ $OBJ ] # FILE.s19
 then
-    OFILE="$( echo $* | grep -o '\-[a-z]\{0,\}o [\#0-9a-zA-Z/\.\_\ -]*\.s19' | cut --delimiter=' ' -f 2- )"    
+    OFILE="$( echo $* | grep -o '\-[a-z]\{0,\}o [\#0-9a-zA-Z/\.\_\ -]*\.s19' | cut --delimiter=' ' -f 2- )"
 
     if [ -z "$OFILE" ]
     then
@@ -221,49 +224,64 @@ then
     fi
 
     echo -e "Object file name: $ANSI_GREEN""$OFILE""$ANSI_NOCOLOR"
-    
+
 fi
 
 ## AS12
 if [ $ASSEMBLY ] && [ $FILE ] # ASSEMBLY
 then
-    
+
     if [ ! -d $DRAGON_AS12_PATH ] || [ ! -f $DRAGON_AS12_PATH/$DRAGON_AS ]
     then
 	echo_warning 'No such assembler program, please put your assembler directory in ~/.dragon_12'
 	exit $EABNE
     fi
-    
+
     if [ $OBJ ] && [ $LST ]
     then
 	:
     elif [ $OBJ ]
-    then	
+    then
 	LFILE="$( sed s/\.asm/\.lst/ <<< "${IFILE}" )"
     elif [ $LST ]
     then
-	OFILE="$( sed s/\.asm/\.s19/ <<< "${IFILE}" )"	
+	OFILE="$( sed s/\.asm/\.s19/ <<< "${IFILE}" )"
     else
 	LFILE="$( sed s/\.asm/\.lst/ <<< "${IFILE}" )"
-	OFILE="$( sed s/\.asm/\.s19/ <<< "${IFILE}" )"	
+	OFILE="$( sed s/\.asm/\.s19/ <<< "${IFILE}" )"
     fi
 
     export LOGFILE="$( sed s/\.asm/\.log/ <<< "${IFILE}" )"
+    export WINEDEBUG=fixme-all
 
     echo "$( date )" > "$LOGFILE"
     echo "
     Assembling with $DRAGON_AS"
 
     # Call WINE for assembly execution
-    export WINE_EXE_PROC='WINEDEBUG=fixme-all wine "$DRAGON_AS12_PATH/$DRAGON_AS" "$IFILE" -L"$LFILE" -o"$OFILE" >> "$LOGFILE"'
-    nohup bash -c 'bash -c ${WINE_EXE_PROC@Q}' > /dev/null 2>&1 < /dev/null
+    export WINE_EXE_PROC="wine $DRAGON_AS12_PATH/$DRAGON_AS $IFILE -L$LFILE -o$OFILE  >> $LOGFILE"
+    bash -c '${WINE_EXE_PROC}' > /dev/null 2>&1 < /dev/null
 
     echo "
     $DRAGON_AS results log: "
     cat "$LOGFILE"
+    echo ''
+
+
+    #if [ $SIM ] && [ $ASSEMBLY ] && [ $FILE ]
+    #then
+    #    OFILE="$( sed s/\.asm/\.s19/ <<< "${IFILE}" )"
+    #    echo $OFILE
+    #    echo 'Opening simulator'
+    #    if [ -f $DRAGON_SIMULATOR_PATH/$DRAGON_SIMULATOR ]
+    #    then
+    #        which java && java -jar $DRAGON_SIMULATOR_PATH/$DRAGON_SIMULATOR -s -b $OFILE &# Then execute simulator
+    #        test $? -eq 0 || echo_warning 'Java not found'
+    #    fi
+    #fi
 
     if [ -z "$( grep 'Total errors: 0' "$LOGFILE" )" ] ; then exit $EASE ; fi
-    
+
 elif [ $ASSEMBLY ]
 then
     echo_error 'You need indicate the .ASM file with -f flag'
@@ -287,7 +305,7 @@ then
  $V_LINE   $ANSI_GREEN"'Burn Dragon 12 board with '"$( rev <<< "$OFILE" | cut -f 1 --delimiter='/' | rev ) : $ANSI_YELLOW	                   $V_LINE
  $INTE_L$H_LINE1$INTE_R$ANSI_NOCOLOR"
     screen -S $DRAGON_SESSION_NAME -X stuff 'load\r'
-    TAMS19FILE=$( wc "$OFILE" | sed 's/ * / /g' | cut -f 2 --delimiter=' ' )    
+    TAMS19FILE=$( wc "$OFILE" | sed 's/ * / /g' | cut -f 2 --delimiter=' ' )
     COUNTERS19=0
     DIRECTION_BEG=0
     DIRECTION_END=0
@@ -297,22 +315,22 @@ then
 	sleep $DRAGON_WAIT_SEND_LINE
 	LINES19_P="$( sed 's/^S./& /g;s/ ../& /;s/ [0-9A-F]\{4\}/& /;s/.\{3\}$/ &/;s/  / /' <<< $LINES19 )"
 	if [ "$COUNTERS19" == '1' ] && [ "48" -lt "$( echo "$LINES19_P" | wc -c )" ]
-	then		
-		LINES19_P="${LINES19_P:0:42} ..."	
+	then
+		LINES19_P="${LINES19_P:0:42} ..."
 	fi
 	DIRECTION_BEG=$(( 0X${LINES19:4:4} ))
-	DIRECTION_END=$(( $DIRECTION_BEG + 0X${LINES19:2:2} - 3 ))	
+	DIRECTION_END=$(( $DIRECTION_BEG + 0X${LINES19:2:2} - 3 ))
 	if (( $DIRECTION_END > $DRAGON_RAM_END )) || (( $DIRECTION_BEG < $DRAGON_RAM_BEG ))
 	then
 	    if [ "${LINES19:0:2}" == 'S1' ]
 	    then
 		echo -e "$ANSI_YELLOW"' +$H_LINE2+'"$ANSI_NOCOLOR"
-		echo -en '    '"$ANSI_YELLOW"'WARNING: Direction is out of RAM range\n    You really want to flash that space? [yes/NO] '"$ANSI_NOCOLOR"		
-		read CMD		
+		echo -en '    '"$ANSI_YELLOW"'WARNING: Direction is out of RAM range\n    You really want to flash that space? [yes/NO] '"$ANSI_NOCOLOR"
+		read CMD
 		if [ "${CMD,,}" == 'yes' ]
 		then
 		    :
-		else		    
+		else
 		    screen -S $DRAGON_SESSION_NAME -X stuff "$( echo S9030000FC )"
 		    exit 0 # That is not an error, it is an option, stop burning
 		fi
@@ -326,7 +344,7 @@ then
 	echo -e '\r'"$ANSI_YELLOW""$PERCENT"'\r'
 
 	echo -ne "$ANSI_YELLOW $BL_COR$H_LINE2$BR_COR$ANSI_NOCOLOR"'\r'
-	
+
 	screen -S $DRAGON_SESSION_NAME -X stuff "$( echo $LINES19 | tr -d '\r' )"
     done
     echo -e "$ANSI_YELLOW $BL_COR$H_LINE2$BR_COR$ANSI_NOCOLOR"
@@ -340,8 +358,8 @@ elif [ $BURN ]
 fi
 
 if [ $RUN ] # Instruction pointer / Program counter
-then    
-    INITIAL_PC="$( echo $* | grep -o '\-[a-z]\{0,\}g [A-F0-9]\{1,4\}' | cut --delimiter=' ' -f 2 )"    
+then
+    INITIAL_PC="$( echo $* | grep -o '\-[a-z]\{0,\}g [A-F0-9]\{1,4\}' | cut --delimiter=' ' -f 2 )"
 
     if [ -z "$INITIAL_PC" ]
     then
@@ -350,19 +368,19 @@ then
     else
 	:
     fi
- 
+
     screen -S $DRAGON_SESSION_NAME -X stuff "g $INITIAL_PC"'\r'
-    
+
     echo "
     PC initial value: $INITIAL_PC
     RUNNING
-    "    
-    
+    "
+
 fi
 
 if [ $SENDCHAR ] && [ ! $SCHARLOOP ] # Send char to TTYUSBX/ttySX
 then
-    CHAR="$( echo $* | grep -o '\-[a-z]\{0,\}c [\.SRa-z0-9]\{1,\}' | cut --delimiter=' ' -f 2- )"    
+    CHAR="$( echo $* | grep -o '\-[a-z]\{0,\}c [\.SRa-z0-9]\{1,\}' | cut --delimiter=' ' -f 2- )"
 
     if [ -z "$CHAR" ]
     then
@@ -376,7 +394,7 @@ then
     do
 	sleep $DRAGON_WAIT_COMMAND
 	screen -S $DRAGON_SESSION_NAME -X stuff "$( echo $TEMP_STRING | tr '@' '\r' | tr 'S' ' ' )"
-	echo " $( sed 's/@/ %CARRIERETURN% /g' <<< $TEMP_STRING | sed 's/S/ %SPACE% /g' ) => $DRAGON_SESSION_NAME"	
+	echo " $( sed 's/@/ %CARRIERETURN% /g' <<< $TEMP_STRING | sed 's/S/ %SPACE% /g' ) => $DRAGON_SESSION_NAME"
     done
 fi
 
@@ -388,35 +406,53 @@ then
     	echo -n "    Insert the string/char => "
 	read CHAR
 	CHAR=$( sed 's/ * / /g;s/ R /R/g' <<< $CHAR | tr ' ' 'S' )
-	
-	if [ "$CHAR" == 'E' ] ; then exit 0 ; fi     
+
+	if [ "$CHAR" == 'E' ] ; then exit 0 ; fi
 
 	    for TEMP_STRING in $( sed 's/R/@R/g' <<< $CHAR | tr 'R' '\n' )
 	    do
 		sleep $DRAGON_WAIT_COMMAND
 		screen -S $DRAGON_SESSION_NAME -X stuff "$( echo $TEMP_STRING | tr '@' '\r' | tr 'S' ' ' )"
-		echo " $( sed 's/@/ %CARRIERETURN% /g;s/S/ %SPACE% /g' <<< $TEMP_STRING ) => $DRAGON_SESSION_NAME"	
+		echo " $( sed 's/@/ %CARRIERETURN% /g;s/S/ %SPACE% /g' <<< $TEMP_STRING ) => $DRAGON_SESSION_NAME"
 	    done
     done
 fi
 
+if [ $OBJ ] && [ $SIM ]
+then
+    echo 'Opening simulator'
+    if [ -f $DRAGON_SIMULATOR_PATH/$DRAGON_SIMULATOR ]
+    then
+        which java && java -jar $DRAGON_SIMULATOR_PATH/$DRAGON_SIMULATOR -s -b $OFILE # Then execute simulator
+        test $? -eq 0 || echo_warning 'Java not found'
+    fi
+fi
+
+if [ $SIM ] && [ $ASSEMBLY ] && [ $FILE ]
+then
+    OFILE="$( sed s/\.asm/\.s19/ <<< "${IFILE}" )"
+    echo $OFILE
+    echo 'Opening simulator'
+    if [ -f $DRAGON_SIMULATOR_PATH/$DRAGON_SIMULATOR ]
+    then
+        which java && java -jar $DRAGON_SIMULATOR_PATH/$DRAGON_SIMULATOR -s -b $OFILE &# Then execute simulator
+        test $? -eq 0 || echo_warning 'Java not found'
+    fi
+fi
 ## Main process, subcommands
 
-case $SUB_COMM in
-    simulator) # Open simulator
-	echo 'Opening simulator'
-	cd $DRAGON_SIMULATOR_PATH  # First go where is configuration file
-	if [ -f $DRAGON_SIMULATOR ]
-	then
-	    which java && java -jar $DRAGON_SIMULATOR # Then execute simulator
-	    test $? -eq 0 || echo_warning 'Java not found'
-	fi
-	cd - # Return
-	;;
-    [!-]*)
-	echo_help
-	;;
-esac
+#case $SUB_COMM in
+#    simulator) # Open simulator
+#	echo 'Opening simulator'
+#	cd $DRAGON_SIMULATOR_PATH  # First go where is configuration file
+#	if [ -f $DRAGON_SIMULATOR ]
+#	then
+#	    which java && java -jar $DRAGON_SIMULATOR # Then execute simulator
+#	    test $? -eq 0 || echo_warning 'Java not found'
+#	fi
+#	cd - # Return
+#
+#esac
 
 # All is done, and it have no errors
 exit 0
